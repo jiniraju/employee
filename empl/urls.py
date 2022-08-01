14d42e97b92dcf89c5ffd95b6da5905e330ad73a
jiniraju <jiniraju1999@gmail.com>
2022-08-01 14:09:46 +0530

    first commit

---
+++ empl/urls.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from django.urls import path
+from empl import views
+from django.urls import include
+from django.views.generic.base import TemplateView
+from django.conf import settings 
+from django.conf.urls.static import static 
+from empl.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    #Company paths 
+    path('comp', views.comp),
+    path('show', views.show),
+    path('edit/<str:cName>', views.edit),
+    path('update/<str:cName>', views.update),
+    path('delete/<str:cName>', views.delete), 
+
+    #employee paths
+    path('emp', views.emp),
+    path('showemp', views.showemp),
+    path('deleteEmp/<str:eFname>', views.deleteEmp),
+    path('editemp/<str:eFname>', views.editemp), 
+    path('updateEmp/<str:eFname>', views.updateEmp),
+
+    #Homepage path
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+
+    #inbuilt login path
+    path('accounts/', include('django.contrib.auth.urls')),
+]
+#for Media Storage 
+if settings.DEBUG: 
+        urlpatterns += static(settings.MEDIA_URL, 
+                              document_root=settings.MEDIA_ROOT) 
